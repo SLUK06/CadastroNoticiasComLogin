7d22c07978c96f37eb2985b12e9a818d43689dda
lokos <lokosamuca23@gmail.com>
2023-10-19 09:20:24 -0300

    Adicionado Comentários

--- Pages/Login.php
+++ Pages/Login.php
@@ -3,34 +3,45 @@
 
     $loginInvalido = "";
 
+    // Verifica se o Metodo de Request é POST
     if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        // Armazena os Valores dos Inputs nas Váriaveis 
         $usuario = $_POST["Usuario"];
         $senha = $_POST["Senha"];
 
+        // Realiza a Busca Pelo Usuário no Banco de Dados
         $sql = "SELECT `id`, `nome`, `nivel`, `usuario`, `email`, `senha` FROM  `usuarios` WHERE (BINARY `usuario` = ? OR BINARY `email` = ?) AND `ativo` = 1 LIMIT 1";
         $stmt = $Conn->prepare($sql);
         $stmt->bind_param("ss", $usuario, $usuario);
         $stmt->execute();
         $result = $stmt->get_result();
 
+        // Verifica se o Usuário Existe
         if($result->num_rows !== 1){
             $loginInvalido = "Usuário ou Senha Estão Incorretos!";
         }else{
             $resultado = $result->fetch_assoc();
 
+            // Verifica se a Senha Inserida no Login Confere com o Hash do Banco de Dados
             if(password_verify($senha, $resultado['senha'])){
+
+                // Caso Conferir Inicia a Sessão
                 if(!isset($_SESSION)) session_start();
 
+                // Seta as Váriaveis de Sessão
                 $_SESSION['UsuarioID'] = $resultado['id'];
                 $_SESSION['UsuarioNome'] = $resultado['nome'];
                 $_SESSION['UsuarioUser'] = $resultado['usuario'];
                 $_SESSION['UsuarioEmail'] = $resultado['email'];
                 $_SESSION['UsuarioNivel'] = $resultado['nivel'];
 
+                // Redireciona o Usuário Para a Página Conta
                 header("Location: Conta.php?aba=minhasPublicacoes");
                 exit;
 
             } else {
+                // Imprime Caso a Senha não Conferir Com o Hash do Banco de Dados
                 $loginInvalido = "Usuário ou Senha Estão Incorretos!";
             }
 
